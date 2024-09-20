bd68bf84ce119b41791e108c010f135b354ae1dc
AmaraaTs <amarjargal.ts01@gmail.com>
2024-09-20 12:32:41 +0800

    mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,20 +4,27 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import authRoute from "./routes/auth-route";
+import { connectDB } from "./config/db";
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || "";
+const MONGO_URI = process.env.MONGO_URI || "";
 
 //express ees application object vvsgeh
 const app = express();
 
 //middlewares
+app.use(express.json());
 app.use("/api/v1/auth", authRoute);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome E-commerce API Server");
 });
 
+connectDB(MONGO_URI);
+
 //server asaah
 app.listen(PORT, () => {
   console.log(`Server localhost:${PORT} running!`);
 });
+
+// MONGOOSE framework san => ODM - Object data mapping
---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,61 @@
+import { model, Schema } from "mongoose";
+
+interface IUser {
+  _id: Schema.Types.ObjectId;
+  firstname: String;
+  lastname: String;
+  email: String;
+  password: String;
+  phoneNumber?: String;
+  role: String;
+  profile_img: String;
+  address: String;
+  created_at: Date;
+  updated_at: Date;
+}
+
+// new gdgiin tsaana baiguulagc function bdg
+const userSchema = new Schema<IUser>({
+  firstname: {
+    type: String,
+    required: [true, "Хэрэглэгчийн нэрийг заавал оруулна уу."],
+  },
+  lastname: {
+    type: String,
+    required: [true, "Хэрэглэгчийн овгийг заавал оруулна уу."],
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Хэрэглэгчийн email заавал оруулна уу."],
+  },
+  password: {
+    type: String,
+    minlength: [8, "Хэрэглэгчийн нууц үг хамгийн багадаа 8 тэмдэгт байна."],
+    required: [true, "Хэрэглэгчийн нууц үгийг заавал оруулна уу."],
+  },
+  phoneNumber: String,
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  profile_img: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1726516335449-eaa942fd4c2f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxN3x8fGVufDB8fHx8fA%3D%3D",
+  },
+  address: String,
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = model("User", userSchema);
+
+export default User;
