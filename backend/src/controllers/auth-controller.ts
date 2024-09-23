9241e875f8ee7abc3e2596d5e3e464698eb95655
AmaraaTs <amarjargal.ts01@gmail.com>
2024-09-23 12:22:45 +0800

    auth

--- backend/src/controllers/auth-controller.ts
+++ backend/src/controllers/auth-controller.ts
@@ -1,26 +1,53 @@
 import { Request, Response } from "express";
 import User from "../models/user.model";
-
-export const login = (req: Request, res: Response) => {
-  res.status(200).json({ message: "Login success" });
-};
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 export const signup = async (req: Request, res: Response) => {
   try {
-    const { name, email, password } = req.body;
+    const { firstname, lastname, email, password } = req.body;
 
-    if (!name || !email || !password) {
-      res.status(400).json({ message: "Хоосон утга байж болохгүй." });
+    if (!firstname || !lastname || !email || !password) {
+      return res.status(400).json({ message: "Хоосон утга байж болохгүй." });
     }
 
     const createdUser = await User.create({
-      name,
+      firstname,
+      lastname,
       email,
       password,
       phoneNumber: "",
     });
-    res.status(201).json({ message: "success", user: createdUser });
+    res
+      .status(201)
+      .json({ message: "create user is successfull", user: createdUser });
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      res.status(404).json({ message: "Бүртгэлтэй хэрэглэгч олдсонгүй." });
+    } else {
+      const isCheck = bcrypt.compareSync(password, user.password.toString());
+      if (!isCheck) {
+        res
+          .status(400)
+          .json({ message: "Хэрэглэгчийн и-мэйл эсвэл нууц үг буруу байна." });
+      } else {
+        const token = jwt.sign({ id: user.id }, "JWT_TOKEN_PASS@123", {
+          expiresIn: "1h",
+        });
+        res
+          .status(200)
+          .json({ message: "Хэрэглэгч амжилттай нэвтэрлээ", token });
+      }
+    }
+  } catch (error) {
+    res.status(400).json({ message: "Хэрэглэгч нэвтрэхэд алдаа гарлаа" });
   }
 };
